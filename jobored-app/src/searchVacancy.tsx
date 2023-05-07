2db46a1f801741d7d1bcd9f0136945c1766b7ae4
ullltimate <ullltimate.zp@gmail.com>
2023-05-07 20:58:56 +0300

    feat: add card vacancies with API

--- jobored-app/src/searchVacancy.tsx
+++ jobored-app/src/searchVacancy.tsx
@@ -1,7 +1,46 @@
 import { Flex, Card, Group, Button, Select, Box, TextInput, Text, Image } from "@mantine/core"
-import { NavLink } from "react-router-dom"
+import CardVacancy from "./cardVacancy"
+import { useState, useEffect } from "react";
 
 function SearchVacancy(){
+    const [vacancies, setVacancies] = useState([]);
+
+    async function getToken() {
+        try {
+            const response = await fetch(
+              `https://startup-summer-2023-proxy.onrender.com/2.0/oauth2/password/?login=sergei.stralenia@gmail.com&password=paralect123&client_id=2356&client_secret=v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948&hr=0`, {
+                headers: {
+                    'x-secret-key': 'GEU4nvd3rej*jeh.eqp'
+                }
+              }
+            );
+            const result = await response.json();
+            return result;
+          } catch (e) {
+            console.log(e);
+          }
+    }
+
+    useEffect(() => {
+        const fetchVacancies = async () => {
+            const tokenObj = await getToken();
+            const accessToken = tokenObj.access_token;
+            const response = await fetch(
+                `https://startup-summer-2023-proxy.onrender.com/2.0/vacancies/?count=4&page=1`, {
+                    headers: {
+                        'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
+                        'X-Api-App-Id': 'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                  }
+            );
+            const data = await response.json();
+            console.log(data.objects);
+            setVacancies(data.objects);
+         };
+         fetchVacancies();
+     }, []);
+
     return (
         <>
             <Flex columnGap={28}>
@@ -48,23 +87,7 @@ function SearchVacancy(){
                   rightSection={<Button color='#5E96FC' compact ml={-40}>Поиск</Button>}
                   mb={16}
                 ></TextInput>
-                <Card>
-                  <Group position='apart' mb={12}>
-                    <NavLink to='/'>Менеджер-дизайнер</NavLink>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.9718 2.70846C11.4382 1.93348 12.5618 1.93348 13.0282 2.70847L15.3586 6.58087C15.5262 6.85928 15.7995 7.05784 16.116 7.13116L20.5191 8.15091C21.4002 8.35499 21.7474 9.42356 21.1545 10.1066L18.1918 13.5196C17.9788 13.765 17.8744 14.0863 17.9025 14.41L18.2932 18.9127C18.3714 19.8138 17.4625 20.4742 16.6296 20.1214L12.4681 18.3583C12.1689 18.2316 11.8311 18.2316 11.5319 18.3583L7.37038 20.1214C6.53754 20.4742 5.62856 19.8138 5.70677 18.9127L6.09754 14.41C6.12563 14.0863 6.02124 13.765 5.80823 13.5196L2.8455 10.1066C2.25257 9.42356 2.59977 8.35499 3.48095 8.15091L7.88397 7.13116C8.20053 7.05784 8.47383 6.85928 8.64138 6.58087L10.9718 2.70846Z" stroke="#ACADB9" stroke-width="1.5"/>
-                    </svg>
-                  </Group>
-                  <Group mb={12}>
-                    <Text fw={700}>з/п от 70000 rub</Text>
-                    <Text>•</Text>
-                    <Text>Полный рабочий день</Text>
-                  </Group>
-                  <Group>
-                    <Image width={20} src='../src/assets/location.svg'></Image>
-                    <Text>Новый Уренгой</Text>
-                  </Group>
-                </Card>
+                {vacancies.map((e: any) => <CardVacancy profession={e.profession} town={e.town.title} paymentto={e.payment_to} paymentfrom={e.payment_from} currency={e.currency} typeWork={e.type_of_work.title}/>)}
               </Box>
             </Flex>
         </>
