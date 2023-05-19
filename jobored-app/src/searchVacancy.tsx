2c4ffe5f5185eb7a725a9676a22dc8ae010e8417
ullltimate <ullltimate.zp@gmail.com>
2023-05-19 18:23:44 +0300

    feat: add filter and search vacancies

--- jobored-app/src/searchVacancy.tsx
+++ jobored-app/src/searchVacancy.tsx
@@ -23,6 +23,11 @@ function SearchVacancy(){
     let date = new Date();
     let todayTimestamp = date.getTime()/1000;
     let [searchQuery, setSearchQuery] = useState('');
+    let [textInput, setTextInput] = useState('');
+    let [keySelect, setKeySelect] = useState('');
+    let [valuePeymentFrom, setValuePeymentFrom] = useState('');
+    let [valuePeymentTo, setValuePeymentTo] = useState('');
+    let [totalVac, setTotalVac] = useState(500);
 
     useEffect(() => {
         const fetchVacancies = async (page:number, searchValue?:string) => {
@@ -39,7 +44,7 @@ function SearchVacancy(){
             }
             setLoader(true);
             const response = await fetch(
-                `https://startup-summer-2023-proxy.onrender.com/2.0/vacancies/?count=4&page=${page-1}&keyword=${searchValue}`, {
+                `https://startup-summer-2023-proxy.onrender.com/2.0/vacancies/?count=4&page=${page-1}&published=1&no_agreement=1${searchValue}`, {
                     headers: {
                         'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
                         'X-Api-App-Id': 'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
@@ -48,9 +53,10 @@ function SearchVacancy(){
                   }
             );
             const data = await response.json();
-            console.log(data.objects);
+            console.log(data.total);
             setVacancies(data.objects);
             setLoader(false);
+            setTotalVac(data.total);
         };
         fetchVacancies(activePage, searchQuery);
     }, [activePage, searchQuery]);
@@ -83,7 +89,9 @@ function SearchVacancy(){
         };
         fetchCategories();
     }, []);
-
+    function createSearchParams(categ: string, peyFrom: string, peyTo: string, searchValue: string){
+      setSearchQuery(`&keyword=${searchValue}&catalogues=${categ}&payment_from=${peyFrom}&payment_to=${peyTo}`)
+    }
     return (
         <>
             <Flex columnGap={28}>
@@ -95,8 +103,13 @@ function SearchVacancy(){
                   data-elem='industry-select'
                   label='Отрасль' 
                   placeholder='Выберете отрасль'
-                  data={categoties.map((e:any)=>e.title_rus)}
+                  data={categoties.map((e:any) => e.title_trimmed)}
                   mb={20}
+                  onChange={(elem:any) => {categoties.map((e:any)=>{
+                    if(elem === e.title_trimmed){
+                      setKeySelect(e.key)
+                    }
+                  })}}
                 >
                 </Select>
                 <NumberInput
@@ -106,6 +119,7 @@ function SearchVacancy(){
                   step={1000}
                   min={0}
                   mb={8}
+                  onChange={(e) => setValuePeymentFrom(String(e))}
                 >
                 </NumberInput>
                 <NumberInput
@@ -114,9 +128,10 @@ function SearchVacancy(){
                   step={1000}
                   min={0}
                   mb={20}
+                  onChange={(e) => setValuePeymentTo(String(e))}
                 >
                 </NumberInput>
-                <Button color='#5E96FC' fullWidth data-elem='search-button'>
+                <Button color='#5E96FC' fullWidth data-elem='search-button' onClick={() => createSearchParams(keySelect, valuePeymentFrom, valuePeymentTo, textInput)}>
                   Применить
                 </Button>
               </Card>
@@ -124,11 +139,11 @@ function SearchVacancy(){
                 <TextInput 
                   data-elem='search-input'
                   placeholder='Введите название вакансии'
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
+                  value={textInput}
+                  onChange={(e) => setTextInput(e.target.value)}
                   display={'block'}
                   icon={<Image width={16} src='../src/assets/search.svg'></Image>}
-                  rightSection={<Button color='#5E96FC' compact ml={-40} data-elem='search-button'>Поиск</Button>}
+                  rightSection={<Button color='#5E96FC' compact ml={-40} data-elem='search-button' onClick={() => createSearchParams(keySelect, valuePeymentFrom, valuePeymentTo, textInput)}>Поиск</Button>}
                   mb={16}
                 ></TextInput>
                 {
@@ -136,7 +151,7 @@ function SearchVacancy(){
                   ? <Center h={500}><Loader /></Center>
                   : vacancies.map((e: any) => <CardVacancy profession={e.profession} town={e.town.title} paymentto={e.payment_to} paymentfrom={e.payment_from} currency={e.currency} typeWork={e.type_of_work.title} id={e.id}/>)
                 }
-                <Pagination value={activePage} onChange={setPage} total={500/4} position="center" />
+                <Pagination value={activePage} onChange={setPage} total={totalVac>500 ? 500/4 : Math.ceil(totalVac/4)} position="center" />
               </Box>
             </Flex>
         </>
